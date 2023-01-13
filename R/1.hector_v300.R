49d15cffe3ad562d5ab6f88aba43634e76a7dd14
kdorheim <kalyn.dorheim@pnnl.gov>
2023-01-13 18:01:01 -0500

    it runs

--- R/1.hector_v300.R
+++ R/1.hector_v300.R
@@ -2,10 +2,12 @@
 # to plot with the Hector v3 output data. 
 # 0. Set Up --------------------------------------------------------------------------------
 # Should be the developmental branch
-#devtools::install_github("jgcri/hector@luc")
 #library(hector)
-devtools::load_all("~/Documents/Hector-Versions/luc/hector")
+library(assertthat)
+HECTOR_V3_DIR <- "~/Documents/Hector-Versions/v3/hector"
+devtools::load_all(HECTOR_V3_DIR)
 library(dplyr)
+library(tidyr)
 
 BASE_DIR <- here::here()
 OUTPUT_DIR <- file.path(BASE_DIR, "output", "hector_output")
@@ -15,19 +17,19 @@ assertthat::assert_that(version == "3.0.0")
 
 source(file.path(BASE_DIR, "R", "0B.functions.R"))
 
-# 1. Run & Save Hector ---------------------------------------------------------------------
+# 1. Emission Driven Hector ---------------------------------------------------------------------
 # Years & variables to save. 
 yrs <- 1750:2100
 vars <- c(CONCENTRATIONS_CO2(), GLOBAL_TAS(), HEAT_FLUX(), RF_TOTAL(), RF_ALBEDO(), 
-          RF_CO2(), RF_SO2(), RF_OC(), RF_BC(), LAND_TAS(), SST(), FFI_EMISSIONS(), DACCS_UPTAKE(), 
-          LUC_EMISSIONS(), LUC_UPTAKE())
+          RF_CO2(), RF_SO2(), RF_OC(), RF_BC(), LAND_TAS(), SST(), FFI_EMISSIONS(),
+          DACCS_UPTAKE(), LUC_EMISSIONS(), LUC_UPTAKE(), NBP())
 params <- c(ECS(), DIFFUSIVITY(), AERO_SCALE(), VOLCANIC_SCALE(), BETA(), Q10_RH())
 
 # Run Hector v3 with the SSP scenarios 
-file.path(BASE_DIR, "input") %>%  
+file.path(HECTOR_V3_DIR, "inst", "input") %>% 
   list.files(pattern = "ssp", full.names = TRUE) %>% 
   lapply(function(f){
-    name <- gsub(pattern = "rcmip_|.ini", replacement = "", x = basename(f))
+    name <- gsub(pattern = "hector_|.ini", replacement = "", x = basename(f))
     core <- newcore(f, name = name)
     run(core)
     out1 <- fetchvars(core, dates = yrs, vars = vars)
@@ -49,11 +51,11 @@ file.path(BASE_DIR, "input") %>%
 
 version <- packageVersion(pkg = "hector")
 rstls$version <- version
-  
+
 file <- paste0("hector_", version, "_ssp.csv")
 write.csv(rstls, file = file.path(OUTPUT_DIR, file), row.names = FALSE)
 
-# Run Hector v3 with the rcp scenarios for the RCP scenarios
+# 2. Run Emission Driven RCP Hector --------------------------------------------------------
 file.path(BASE_DIR, "input") %>%  
   list.files(pattern = "RCP", full.names = TRUE) %>% 
   lapply(function(f){
@@ -72,4 +74,55 @@ version <- packageVersion(pkg = "hector")
 rstls$version <- version
 
 file <- paste0("hector_", version, "_rcp.csv")
+write.csv(rstls, file = file.path(OUTPUT_DIR, file), row.names = FALSE)
+
+
+# 3. Run Concentration Driven RCP Hector --------------------------------------------------------
+
+# Years & variables to save. 
+yrs <- 1750:2100
+vars <- c(CONCENTRATIONS_CO2(), GLOBAL_TAS(), HEAT_FLUX(), RF_TOTAL(), RF_ALBEDO(), 
+          RF_CO2(), RF_SO2(), RF_OC(), RF_BC(), LAND_TAS(), SST(), FFI_EMISSIONS(),
+          DACCS_UPTAKE(), LUC_EMISSIONS(), LUC_UPTAKE(), NBP())
+params <- c(ECS(), DIFFUSIVITY(), AERO_SCALE(), VOLCANIC_SCALE(), BETA(), Q10_RH())
+
+
+file.path(HECTOR_V3_DIR, "inst", "input") %>% 
+  list.files(pattern = "ssp", full.names = TRUE) %>%
+  lapply(function(ini_file){
+    scn <- gsub(x = basename(ini_file), pattern = "hector_|.ini", replacement = "" )
+    hc <- newcore(ini_file, name = scn)
+    csv_file <- list.files(file.path(dirname(ini_file), "tables"), pattern = scn, full.names = TRUE)
+    data <- read.csv(csv_file, comment.char = ";")
+    
+    conc_constraints <- c("C2F6_constrain", "CCl4_constrain", "CF4_constrain", "CFC113_constrain",   
+                          "CFC114_constrain", "CFC115_constrain", "CFC11_constrain", "CFC12_constrain", 
+                          "CH3Br_constrain", "CH3Cl_constrain", "CH4_constrain", "CO2_constrain", 
+                          "HCFC141b_constrain", "HCFC142b_constrain", "HCFC22_constrain", "HFC125_constrain", 
+                          "HFC134a_constrain", "HFC143a_constrain", "HFC227ea_constrain", "HFC23_constrain",
+                          "HFC32_constrain", "HFC4310_constrain", 
+                          "N2O_constrain", "SF6_constrain", "halon1211_constrain", "halon1301_constrain", 
+                          "halon2402_constrain")
+    
+    data[, c("Date", conc_constraints)] %>% 
+      pivot_longer(cols = conc_constraints) %>%  
+      mutate(units = getunits(name)) ->
+      conc_inputs_hc
+    
+    name <- paste0(gsub(pattern = "hector_|.ini", replacement = "", x = basename(ini_file)), " conc")
+    hc <- newcore(inifile = ini_file, name = name)
+    setvar(core = hc, dates = conc_inputs_hc$Date, var = conc_inputs_hc$name, 
+           values = conc_inputs_hc$value, unit = conc_inputs_hc$units)
+    reset(hc)
+    run(hc)
+    out <- fetchvars(hc, dates = yrs, vars = vars)
+    return(out)
+  }) %>%  
+  do.call(what = "rbind") ->
+  rstls
+
+version <- packageVersion(pkg = "hector")
+rstls$version <- version
+
+file <- paste0("hector_", version, "_ssp-conc.csv")
 write.csv(rstls, file = file.path(OUTPUT_DIR, file), row.names = FALSE)
