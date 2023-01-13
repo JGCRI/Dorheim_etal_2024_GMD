59f93053530738663b5bccd78c8b706af2a00378
kdorheim <kalyn.dorheim@pnnl.gov>
2023-01-13 17:16:47 -0500

    clean calibration protcol

--- R/0A.hectorv3_calibration.R
+++ R/0A.hectorv3_calibration.R
@@ -12,8 +12,9 @@ library(readxl)
 theme_set(theme_bw())
 
 # TODO update this to the v3 branch after the luc branch is merged in 
-devtools::install_github("jgcri/hector@luc")
-library(hector)
+#devtools::install_github("jgcri/hector@luc")
+#library(hector)
+devtools::load_all("~/Documents/Hector-Versions/v3/hector")
 
 BASE_DIR <- here::here()
 
@@ -43,6 +44,7 @@ set_params <- function(core, p){
 # Return data frame of the global mean surface temperature 
 fetch_gmst <- function(core){
   
+  # This value is from Hector.
   flnd  <- 0.29  
   
   fetchvars(core, dates = 1750:2100, vars = c(LAND_TAS(), SST())) %>% 
@@ -121,16 +123,6 @@ read.csv(file.path(BASE_DIR, "data", "calibration", "co2_annmean_mlo.csv"), comm
   mutate(variable = CONCENTRATIONS_CO2()) -> 
   co2_obs_data
 
-# Friedlingstein, Pierre, O'Sullivan, Michael, Jones, Matthew W., Andrew, Robbie M., Hauck, Judith, Olsen,
-# Are, Peters, Glen P., Peters, Wouter, Pongratz, Julia, Sitch, Stephen, Le Quéré, Corinne. et al. 
-# Carbon Budget 2021, Earth Syst. Sci. Data, 2022. https://doi.org/10.5194/essd-14-1917-2022
-read_xlsx(file.path(BASE_DIR, "data", "calibration", "Global_Carbon_Budget_2021v1.0.xlsx"), 
-          sheet = "Historical Budget", skip = 15) %>% 
-  select(year = Year, value = `land sink`) %>% 
-  mutate(variable = "land sink") -> 
-  land_obs_data
-
-
 # 1. Calibrate Hector -----------------------------------------------------------------------------
 # Calculate the MSE between Hector output and observations, this function will be used by optim to 
 # minimze the MSE ()
@@ -138,8 +130,7 @@ read_xlsx(file.path(BASE_DIR, "data", "calibration", "Global_Carbon_Budget_2021v
 #   p: vector of Hector parameter values to calibrate, must be named 
 #   gmst_obs: data frame of the gmst observations from NASA GISS
 #   co2_obs: data frame of the CO2 concentrations NOAA 
-#   land_obs: data frame of the land sink Global Carbon Project
-get_mse <- function(p, gmst_obs, co2_obs, land_obs){
+get_mse <- function(p, gmst_obs, co2_obs){
   
 
   # Prevent an error from crashing the fit, if for some reason (randomly generated parameter value results in an error)
@@ -167,15 +158,8 @@ get_mse <- function(p, gmst_obs, co2_obs, land_obs){
     hector_co2 <- fetchvars(hc, dates = co2_obs$year, vars = CONCENTRATIONS_CO2())
     MSE_co2 <-mean((hector_co2$value - co2_obs$value)^2)
     
-    # Calculate the size of Hector's land sink & calculate the normalized MSE. 
-    fetchvars(hc, dates = land_obs$year, vars = c(NBP(), LUC_EMISSIONS())) %>%  
-      spread(variable, value) %>%
-      mutate(value = NBP + luc_emissions) ->
-      hector_land
-    MSE_land <- mean((hector_land$value - land_obs$value)^2)
-    
     # The goodness of fit is the mean of the normalized MSEs 
-    MSEs <- c(MSE_gmst, MSE_co2, MSE_land)
+    MSEs <- c(MSE_gmst, MSE_co2)
     
     return(mean(MSEs))
     
@@ -188,203 +172,24 @@ get_mse <- function(p, gmst_obs, co2_obs, land_obs){
   
 }
 
+
+
 params <- c("diff" = 1, "q10_rh" = 2.2, "beta" = 0.23)
-fit <- optim(get_mse, p = params, gmst_obs = gmst_obs_data,
-             co2_obs = co2_obs_data, land_obs = land_obs_data)
+fit <- optim(get_mse, 
+             p = params,
+             gmst_obs = gmst_obs_data,
+             co2_obs = co2_obs_data)
 
-save(fit, file = file.path(BASE_DIR, "output", paste0("calibration-fit-", gsub(date(), pattern = " ", replacement = "_"), ".rda")))
+save(fit, file = file.path(BASE_DIR, "output", paste0("calibration-", gsub(date(), pattern = " ", replacement = "_"), ".rda")))
 
-# # Variations of Hector Calibration  -----------------------------------------------------------------------------
-# # No Land Sink Constraint -----------------------------------------------------------------------------
-# 
-# # Calculate the MSE between Hector output and observations, this function will be used by optim to 
-# # minimze the MSE ()
-# # Args 
-# #   p: vector of Hector parameter values to calibrate, must be named 
-# #   gmst_obs: data frame of the gmst observations from NASA GISS
-# #   co2_obs: data frame of the CO2 concentrations NOAA 
-# get_mse <- function(p, gmst_obs, co2_obs){
-#   
-#   
-#   # Prevent an error from crashing the fit, if for some reason (randomly generated parameter value results in an error)
-#   # return a large MSE value. 
-#   out <- tryCatch({
-#     
-#     # Make sure that the two boundary scenarios run, otherwise there could be issues if the parameter 
-#     # combination results in error conditions in the future. 
-#     hc <- prep_core("rcmip_ssp119.ini")
-#     set_params(hc, p)
-#     reset(hc)
-#     run(hc)
-#     
-#     hc <- prep_core("rcmip_ssp585.ini")
-#     set_params(hc, p)
-#     reset(hc)
-#     run(hc)
-#     
-#     # Fetch the gmst results from the Hector core & calculate the normalized MSE. 
-#     hector_gmst <- normalize_to_giss(fetch_gmst(hc))
-#     hector_gmst <- hector_gmst[hector_gmst$year %in% gmst_obs$year, ]
-#     MSE_gmst <- mean((hector_gmst$value - gmst_obs$value)^2/abs(hector_gmst$value))
-#     
-#     # Fetch the co2 results from the Hector core & calculate the normalized MSE. 
-#     hector_co2 <- fetchvars(hc, dates = co2_obs$year, vars = CONCENTRATIONS_CO2())
-#     MSE_co2 <-mean((hector_co2$value - co2_obs$value)^2/abs(hector_co2$value))
-#     
-#     # The goodness of fit is the mean of the normalized MSEs 
-#     MSEs <- c(MSE_gmst, MSE_co2)
-#     
-#     return(mean(MSEs))
-#     
-#   }, error = function(e){
-#     # If an error was thrown return a large value
-#     return(9999)
-#   })
-#   
-#   return(out)
-#   
-# }
-# 
-# params <- c("diff" = 1, "q10_rh" = 2.2, "beta" = 0.23)
-# fit <- optim(get_mse, p = params, gmst_obs = gmst_obs_data, co2_obs = co2_obs_data)
-# save(fit, file = file.path(BASE_DIR, "output", paste0("no_land_sink-calibration-fit-", gsub(date(), pattern = " ", replacement = "_"), ".rda")))
-# 
-# 
-# 
-# # CO2 Concentrations Double weighted   -----------------------------------------------------------------------------
-# 
-# # Calculate the MSE between Hector output and observations, this function will be used by optim to 
-# # minimze the MSE ()
-# # Args 
-# #   p: vector of Hector parameter values to calibrate, must be named 
-# #   gmst_obs: data frame of the gmst observations from NASA GISS
-# #   co2_obs: data frame of the CO2 concentrations NOAA 
-# #   land_obs: data frame of the land sink Global Carbon Project
-# get_mse <- function(p, gmst_obs, co2_obs, land_obs){
-#   
-#   
-#   # Prevent an error from crashing the fit, if for some reason (randomly generated parameter value results in an error)
-#   # return a large MSE value. 
-#   out <- tryCatch({
-#     
-#     # Make sure that the two boundary scenarios run, otherwise there could be issues if the parameter 
-#     # combination results in error conditions in the future. 
-#     hc <- prep_core("rcmip_ssp119.ini")
-#     set_params(hc, p)
-#     reset(hc)
-#     run(hc)
-#     
-#     hc <- prep_core("rcmip_ssp585.ini")
-#     set_params(hc, p)
-#     reset(hc)
-#     run(hc)
-#     
-#     # Fetch the gmst results from the Hector core & calculate the normalized MSE. 
-#     hector_gmst <- normalize_to_giss(fetch_gmst(hc))
-#     hector_gmst <- hector_gmst[hector_gmst$year %in% gmst_obs$year, ]
-#     MSE_gmst <- mean((hector_gmst$value - gmst_obs$value)^2/abs(hector_gmst$value))
-#     
-#     # Fetch the co2 results from the Hector core & calculate the normalized MSE. 
-#     hector_co2 <- fetchvars(hc, dates = co2_obs$year, vars = CONCENTRATIONS_CO2())
-#     MSE_co2 <-mean((hector_co2$value - co2_obs$value)^2/abs(hector_co2$value))
-#     
-#     # Calculate the size of Hector's land sink & calculate the normalized MSE. 
-#     fetchvars(hc, dates = land_obs$year, vars = c(NBP(), LUC_EMISSIONS())) %>%  
-#       spread(variable, value) %>%
-#       mutate(value = NBP + luc_emissions) ->
-#       hector_land
-#     MSE_land <- mean((hector_land$value - land_obs$value)^2 /abs(hector_land$value))
-#     
-#     # The goodness of fit is the mean of the normalized MSEs 
-#     MSE <- weighted.mean(c(MSE_gmst, MSE_co2, MSE_land), c(1, 2, 1))
-#     return(MSE)
-#     
-#   }, error = function(e){
-#     # If an error was thrown return a large value
-#     return(9999)
-#   })
-#   
-#   return(out)
-#   
-# }
-# 
-# params <- c("diff" = 1, "q10_rh" = 2.2, "beta" = 0.23)
-# fit <- optim(get_mse, p = params, gmst_obs = gmst_obs_data,
-#              co2_obs = co2_obs_data, land_obs = land_obs_data)
-# 
-# save(fit, file = file.path(BASE_DIR, "output", paste0("co2_mse_weighted-calibration-fit-", gsub(date(), pattern = " ", replacement = "_"), ".rda")))
-# 
-# 
-# # MSE not percent diff  -----------------------------------------------------------------------------
-# 
-# # Calculate the MSE between Hector output and observations, this function will be used by optim to 
-# # minimze the MSE ()
-# # Args 
-# #   p: vector of Hector parameter values to calibrate, must be named 
-# #   gmst_obs: data frame of the gmst observations from NASA GISS
-# #   co2_obs: data frame of the CO2 concentrations NOAA 
-# #   land_obs: data frame of the land sink Global Carbon Project
-# get_mse <- function(p, gmst_obs, co2_obs, land_obs){
-#   
-#   
-#   # Prevent an error from crashing the fit, if for some reason (randomly generated parameter value results in an error)
-#   # return a large MSE value. 
-#   out <- tryCatch({
-#     
-#     # Make sure that the two boundary scenarios run, otherwise there could be issues if the parameter 
-#     # combination results in error conditions in the future. 
-#     hc <- prep_core("rcmip_ssp119.ini")
-#     set_params(hc, p)
-#     reset(hc)
-#     run(hc)
-#     
-#     hc <- prep_core("rcmip_ssp585.ini")
-#     set_params(hc, p)
-#     reset(hc)
-#     run(hc)
-#     
-#     # Fetch the gmst results from the Hector core & calculate the normalized MSE. 
-#     hector_gmst <- normalize_to_giss(fetch_gmst(hc))
-#     hector_gmst <- hector_gmst[hector_gmst$year %in% gmst_obs$year, ]
-#     MSE_gmst <- mean((hector_gmst$value - gmst_obs$value)^2)
-#     
-#     # Fetch the co2 results from the Hector core & calculate the normalized MSE. 
-#     hector_co2 <- fetchvars(hc, dates = co2_obs$year, vars = CONCENTRATIONS_CO2())
-#     MSE_co2 <-mean((hector_co2$value - co2_obs$value)^2)
-#     
-#     # Calculate the size of Hector's land sink & calculate the normalized MSE. 
-#     fetchvars(hc, dates = land_obs$year, vars = c(NBP(), LUC_EMISSIONS())) %>%  
-#       spread(variable, value) %>%
-#       mutate(value = NBP + luc_emissions) ->
-#       hector_land
-#     MSE_land <- mean((hector_land$value - land_obs$value)^2)
-#     
-#     # The goodness of fit is the mean of the normalized MSEs 
-#     MSE <- weighted.mean(c(MSE_gmst, MSE_co2, MSE_land), c(1, 1, 1))
-#     return(MSE)
-#     
-#   }, error = function(e){
-#     # If an error was thrown return a large value
-#     return(9999)
-#   })
-#   
-#   return(out)
-#   
-# }
-# 
-# params <- c("diff" = 1, "q10_rh" = 2.2, "beta" = 0.23)
-# fit <- optim(get_mse, p = params, gmst_obs = gmst_obs_data,
-#              co2_obs = co2_obs_data, land_obs = land_obs_data)
-# 
-# save(fit, file = file.path(BASE_DIR, "output", paste0("not-percent-", gsub(date(), pattern = " ", replacement = "_"), ".rda")))
-# 
-# 
 
 # 2. Quick Compare Fit Results with Obs -----------------------------------------------------------------------------
 # Set up, run, and fectch Hector results. 
-hc <- prep_core(filename = "hector_ssp119.ini")
+hc <- prep_core(filename = "rcmip_ssp119.ini")
 set_params(core = hc, p = fit$par)
+reset(hc)
 run(hc)
+
 
 normalize_to_giss(fetch_gmst(hc)) %>% 
   filter(year %in% gmst_obs_data$year) -> 
@@ -393,30 +198,14 @@ normalize_to_giss(fetch_gmst(hc)) %>%
 fetchvars(hc, dates = 1850:2100, vars = CONCENTRATIONS_CO2()) -> 
   hector_co22 
 
-fetchvars(hc, dates = land_obs_data$year, vars = c(NBP(), LUC_EMISSIONS())) %>%  
-  spread(variable, value) %>%
-  mutate(value = NBP + luc_emissions) ->
-  hector_land
-
 ggplot() + 
   geom_line(data = hector_gmst, aes(year, value, color = "hector v3")) + 
   geom_line(data = gmst_obs_data, aes(year, value, color = "NASA GISS")) + 
   labs(title = "Global Mean Surface Temperature", x = NULL)
 
-
-file.path(BASE_DIR, "data", "hector_2.2.0_rcp.csv") %>% 
-  read.csv() %>% 
-  filter(variable == CONCENTRATIONS_CO2() & scenario == "rcp26") -> 
-  old_hector
-
 ggplot() + 
-  geom_line(data = hector_co2, aes(year, value, color = "hector v3")) + 
-  geom_line(data = hector_co22, aes(year, value, color = "hector v3 og")) + 
+  geom_line(data = hector_co22 %>% filter(year %in% 1950:2020), aes(year, value, color = "hector v3 og")) + 
   geom_line(data = co2_obs_data, aes(year, value, color = "NOAA")) + 
-  #geom_line(data = old_hector, aes(year, value, color = "hector v2")) +
   labs(title = "CO2 Concentrations", x = NULL)
 
-ggplot() + 
-  geom_line(data = hector_land, aes(year, value, color = "hector v3")) + 
-  geom_line(data = land_obs_data, aes(year, value, color = "GCB"), alpha = 0.4) + 
-  labs(title = "Land Sink", x = NULL)
+
